67eeb4e47b5aa674e3d17f5eab9d21292caf67c7
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-07 12:37:42 +0100

    add the file Main in app.jsx and create the file on components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
-
+import Main from "./components/Main"
 function App() {
 
   return (
     <>
-
+      <header>
+        <h1>Learn Web developer</h1>
+      </header>
+      <Main />
     </>
   )
 }
